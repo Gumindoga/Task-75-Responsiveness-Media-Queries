8208ac6572282d3236fa1cd8f5d03754c7e2b372
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2022-07-06 17:26:25 +0200

    Updated repo structure

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("View repository at https://github.com/Gumindoga/Task-75-Responsiveness-Media-Queries");
+if (window.confirm("Click OK to view repository, this tab won't close")) {
+	window.open(
+		"https://github.com/Gumindoga/Task-75-Responsiveness-Media-Queries.git"
+	);
+}
